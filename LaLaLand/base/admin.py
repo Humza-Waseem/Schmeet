b0428fac740a3fe59f4ab8359c22eda6be54b352
Hamza <hamza.wasym99@gmail.com>
2026-04-18 01:50:07 +0500

    prev delete

--- LaLaLand/base/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Student, CareerRecommendation, StartupIdea
-
-# Register your models here.
-admin.site.register(Student)
-admin.site.register(CareerRecommendation)
-admin.site.register(StartupIdea)
